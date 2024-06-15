2c377354f4ac8c245f594b058c02b16780848f7f
DanChae03 <118027673+DanChae03@users.noreply.github.com>
2024-06-15 13:37:44 +1200

    feat: basic constant types and enums

---
+++ src/constants/types.d.ts
@@ -0,0 +1,15 @@
+export type Card = {
+  verse: string;
+  body: string;
+  time: string;
+  prev: string;
+  book: string;
+  chapter: number;
+  verse: number;
+  category: string[];
+  keywords: string[];
+  notes: string[];
+  mastery: number;
+  attempts: number;
+  correct: number;
+};
